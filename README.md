60a4cf79cabe740246606f485d49772d4cf67cd3
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2019-12-29 14:29:09 +0100

    pages to read

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ playing around
 1. enable developer mode (right top corner)
 1. `Load Unpacked`
 1. choose this folder
+
+## pages to read
+* https://developer.chrome.com/extensions/getstarted
+* https://www.tomforth.co.uk/chromeextension/
+* https://medium.com/hci-wvu/hello-world-chrome-extension-tutorial-f2a174a5f5c0
--- popup.js
+++ popup.js
@@ -1,5 +1,10 @@
-$(function() {
-  $("#name").keyup(function() {
-    $("#greet").text("Hello " + $("#name").val());
+window.addEventListener("load", function(evt) {
+  chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+    file: "payload.js"
   });
+});
+
+// Listen to messages from the payload.js script and write to popout.html
+chrome.runtime.onMessage.addListener(function(message) {
+  document.getElementById("pagetitle").innerHTML = message;
 });
